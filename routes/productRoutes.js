872234feaef3c7b4fc26610bcd66730c037247f2
unknown <spoortimadlimath@gmail.com>
2025-07-25 10:41:19 +0530

    Product CRUD

---
+++ routes/productRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productControllers")
+
+const productRoutes = express.Router();
+
+productRoutes.get("/products", getAllProducts);
+
+productRoutes.get("/products/:id", getProductById);
+
+productRoutes.post("/products", createProduct);
+
+productRoutes.put("/products/:id", updateProduct);
+
+productRoutes.delete("/products/:id", deleteProduct);
+
+module.exports = productRoutes;
